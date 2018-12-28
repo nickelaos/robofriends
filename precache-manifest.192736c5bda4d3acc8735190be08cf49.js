22e1e8d7519170e0aaaa4ef788ab205fb1e92c6f
nickelaos <nickelaos92@gmail.com>
2018-12-28 23:38:07 +0200

    Updates

---
+++ precache-manifest.192736c5bda4d3acc8735190be08cf49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "68834d77e73584f0c34b",
+    "url": "/robofriends/static/js/main.68834d77.chunk.js"
+  },
+  {
+    "revision": "a68d794e20ea6f5a9854",
+    "url": "/robofriends/static/js/1.a68d794e.chunk.js"
+  },
+  {
+    "revision": "68834d77e73584f0c34b",
+    "url": "/robofriends/static/css/main.8996c53c.chunk.css"
+  },
+  {
+    "revision": "a68d794e20ea6f5a9854",
+    "url": "/robofriends/static/css/1.fb4ff05d.chunk.css"
+  },
+  {
+    "revision": "0ec777801c9afae3b4159b9629bc1b69",
+    "url": "/robofriends/index.html"
+  }
+];
